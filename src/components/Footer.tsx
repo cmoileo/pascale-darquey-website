a48e5a85a2f8ba8502ef5bff16840fb6accda95f
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-01-27 14:44:16 +0100

    init footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,24 @@
 export default function footer() {
     return (
         <footer>
-            <h3>Footer</h3>
+            <form action="submit">
+                <label htmlFor="Prénom">
+                    <input type="text" name="Prénom" id="Prénom" placeholder="Prénom" />
+                </label>
+                <label htmlFor="Nom">
+                    <input type="text" name="Nom" id="Nom" placeholder="Nom" />
+                </label>
+                <label htmlFor="Email">
+                    <input type="email" name="Email" id="Email" placeholder="Email" />
+                </label>
+                <label htmlFor="Message">
+                    <textarea name="Message" id="Message" placeholder="Quel est votre projet ?"></textarea>
+                </label>
+                <button className="main-cta white" type="submit">Envoyer</button>
+            </form>
+            <nav>
+
+            </nav>
         </footer>
     )
 }
