3ef2776c3ae455f1a2dc966b67a1ab426fcf8625
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-01-27 16:13:14 +0100

    section 3

--- src/homeComponents/Section3.tsx
+++ src/homeComponents/Section3.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import Pascale from "../../public/images/Pascale.jpeg"
+
 export default function Section3() {
     return  (
         <section className="section-3">
-            <h1>Section 3</h1>
+            <h2 className='secondary-title orange'>QUI SUIS-JE ?</h2>
+            <Image src={Pascale} alt={"Pascale Darquey souriant et regardant la caméra"}></Image>
         </section>
     )
 }
