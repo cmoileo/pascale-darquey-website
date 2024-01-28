df7b03f86d911dd15ccfbc0b41c8ca4e3bfd14b3
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-01-28 17:55:55 +0100

    finish third section

--- src/homeComponents/Section3.tsx
+++ src/homeComponents/Section3.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Pascale from "../../public/images/Pascale.jpeg"
+import Img from "../../public/images/image 6.png"
 
 export default function Section3() {
     return  (
@@ -8,6 +9,15 @@ export default function Section3() {
                 <h2 className='secondary-title orange'>QUI SUIS-JE ?</h2>
                 <Image src={Pascale} alt={"Pascale Darquey souriant et regardant la caméra"}></Image>
                 <p className="main-parag heavy_blue">En tant que coach expérimentée avec plus de 30 ans d'expérience en management international, je propose un accompagnement individuel et collectif. Ma approche intègre une sensibilité multiculturelle, combinant stratégie, co-développement et pratiques narratives. J'ai travaillé avec des entreprises variées, de l'ESN aux services automobiles, offrant un soutien à la définition de visions stratégiques et à la gestion du changement. En individuel, je renforce les postures professionnelles et explore de nouvelles orientations. Mes séances visent à développer la créativité, la cohésion d'équipe et à optimiser les performances.</p>
+            </div>
+            <div className="second-wrapper">
+                <div className="left">
+                    <h3 className="secondary-title orange">Mon approche du coaching</h3>
+                    <p className="main-parag heavy_blue">Je mets mes expériences et ma sensibilité multiculturelle au service de l’accompagnement des personnes et des équipes. Par une approche systémique, je combine approche stratégique, techniques issues du co-développement, du team-building, des pratiques narratives, en respectant les valeurs et la personnalité des coachés. Je veille à ce que les solutions trouvées servent l’intérêt des coachés et celui de l’écosystème dans lequel ils interagissent.</p>
+                </div>
+                <div className="right">
+                    <Image src={Img} alt={"Coach en train de travailler avec un professionnel"}></Image>
+                </div>
             </div>
         </section>
     )
