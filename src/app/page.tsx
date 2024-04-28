457fb9265a026ac64cc445f7059927f8d3dc014e
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-28 17:57:52 +0200

    quelques corrections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { useEffect } from "react";
-import headerManager from "@/hooks/headerManager";
-import carouselManager from "@/hooks/CarouselManager";
-import useFAQManager from "@/hooks/FAQManager";
-import swiperJS from "@/hooks/swiperJS";
-import offresManager from "@/hooks/offresManager";
-import carouselAutoManager from "@/hooks/CarouselAutoManager";
+import headerManager from "../hooks/headerManager";
+import carouselManager from "../hooks/CarouselManager";
+import useFAQManager from "../hooks/FAQManager";
+import swiperJS from "../hooks/swiperJS";
+import offresManager from "../hooks/offresManager";
+import carouselAutoManager from "../hooks/CarouselAutoManager";
 export default function Home() {
   console.log("home loaded !")
   useEffect(() => {
