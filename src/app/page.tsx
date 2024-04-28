3ba1c1d360271a6807fb988ef6c89df7750d4c3e
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-28 18:09:26 +0200

    quelques corrections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ import offresManager from "../hooks/offresManager";
 import carouselAutoManager from "../hooks/CarouselAutoManager";
 export default function Home() {
   console.log("home loaded 1 without return !")
-  console.log("home loaded 2 !")
-  console.log(headerManager)
-  headerManager()
-  carouselManager()
-  useFAQManager()
-  swiperJS()
-  offresManager()
-  carouselAutoManager();
+  useEffect(() => {
+    console.log("home loaded 2 !")
+    console.log(headerManager)
+    headerManager()
+    carouselManager()
+    useFAQManager()
+    swiperJS()
+    offresManager()
+    carouselAutoManager();
+  }, []);
 }
